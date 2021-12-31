f74d7caddc6e578d8621b54e3d427430ac2a02b2
Kaspars <62926826+KaspijaALT@users.noreply.github.com>
2021-12-31 21:37:38 +0200

    Update guess.py

--- guess.py
+++ guess.py
@@ -1,13 +1,3 @@
-"""Guess a number within a range.
-
-Exercises
-
-1. Change the range to be from 0 to 1,000,000.
-2. Can you still guess the number?
-3. Print the number of guesses made.
-4. Limit the number of guesses to the minimum required.
-"""
-
 from random import randint
 
 start = 1
